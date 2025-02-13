322ec0ddbd595842f9e155624243c19c85698c5f
phial3 <27754514+phial3@users.noreply.github.com>
2025-02-13 14:50:40 +0800

    test examples

--- examples/transcode.rs
+++ examples/transcode.rs
@@ -1,6 +1,6 @@
 use rsmedia::decode::Decoder;
 use rsmedia::encode::Settings;
-use rsmedia::{Encoder, Time};
+use rsmedia::{EncoderBuilder};
 use std::path::Path;
 
 fn main() {
@@ -8,40 +8,22 @@ fn main() {
     let mut decoder = Decoder::new(source).expect("failed to create decoder");
 
     let settings = Settings::preset_h264_yuv420p(1280, 720, false);
-    let mut encoder =
-        Encoder::new(Path::new("rainbow.mp4"), settings).expect("failed to create encoder");
+    let mut encoder = EncoderBuilder::new(Path::new("person.mp4"), settings)
+        .with_format("mp4")
+        .build()
+        .expect("failed to create encoder");
 
-    let duration = Time::from_nth_of_a_second(24);
-    let mut position = Time::zero();
+    for frame in decoder.decode_raw_iter() {
+        if let Ok(raw_frame) = frame {
 
-    // array frame
-    for frame in decoder.decode_iter() {
-        if let Ok((ts, frame)) = frame {
-            let rgb = frame.slice(ndarray::s![0, 0, ..]).to_slice().unwrap();
-            println!(
-                "pixel at 0, 0: {}, {}, {}, time:{:?}",
-                rgb[0], rgb[1], rgb[2], ts
-            );
-            encoder
-                .encode(&frame, position)
-                .expect("failed to encode frame");
+            println!("frame width: {}, height: {}, pix_format: {}, pts:{}, time_base:{:?}",
+                     raw_frame.width, raw_frame.height, raw_frame.format, raw_frame.pts, raw_frame.time_base);
+
+            encoder.encode_raw(&raw_frame).expect("failed to encode frame");
         } else {
             break;
         }
-
-        // Update the current position and add the inter-frame duration to it.
-        position = position.aligned_with(duration).add();
     }
-
-    // raw frame
-    // for frame in decoder.decode_raw_iter() {
-    //     if let Ok(raw_frame) = frame {
-    //         println!("frame width: {}, height: {}, pix_format: {}", raw_frame.width, raw_frame.height, raw_frame.format);
-    //         encoder.encode_raw(&raw_frame).expect("failed to encode frame");
-    //     } else {
-    //         break;
-    //     }
-    // }
 
     encoder.finish().unwrap();
 }
