3b8ca4bbb1a342f90934bcc6eea7ac4e5ff07529
phial3 <27754514+phial3@users.noreply.github.com>
2025-02-14 01:42:46 +0800

    update readme

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod error;
 pub mod frame;
 #[cfg(feature = "ndarray")]
 pub use frame::FrameArray;
+mod ffi_hwaccel;
+mod flags;
 pub mod hwaccel;
 pub mod init;
 pub mod io;
@@ -16,9 +18,6 @@ pub mod resize;
 pub mod stream;
 pub mod time;
 
-mod ffi_hwaccel;
-mod flags;
-
 pub use decode::{Decoder, DecoderBuilder};
 pub use encode::{Encoder, EncoderBuilder};
 pub use init::init;
@@ -28,11 +27,14 @@ pub use options::Options;
 pub use packet::Packet;
 pub use rational::Rational;
 pub use resize::Resize;
+pub use rsmpeg::ffi;
 pub use time::Time;
 
 /// Re-export internal `AvFrame` for caller to use.
 pub type RawFrame = rsmpeg::avutil::AVFrame;
 
 /// pixel format
-pub const PIXEL_FORMAT_RGB24: rsmpeg::ffi::AVPixelFormat = rsmpeg::ffi::AV_PIX_FMT_RGB24;
-pub const PIXEL_FORMAT_YUV420P: rsmpeg::ffi::AVPixelFormat = rsmpeg::ffi::AV_PIX_FMT_YUV420P;
+pub const PIXEL_FORMAT_RGB24: ffi::AVPixelFormat = ffi::AV_PIX_FMT_RGB24;
+pub const PIXEL_FORMAT_YUV420P: ffi::AVPixelFormat = ffi::AV_PIX_FMT_YUV420P;
+/// NV12 pixel format with hardware acceleration
+pub const PIXEL_FORMAT_NV12: ffi::AVPixelFormat = ffi::AV_PIX_FMT_NV12;
