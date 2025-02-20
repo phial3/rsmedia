2d14139ea665b2e4e31f57b7c2156226c0d11d9f
admin <gyf@travelsky.com.cn>
2025-02-20 22:08:01 +0800

    fmt

--- examples/encode_frame.rs
+++ examples/encode_frame.rs
@@ -7,10 +7,9 @@ use std::path::Path;
 fn main() {
     rsmedia::init().unwrap();
 
-    let settings =
-        Settings::preset_h264_yuv420p(1280, 720, false)
-            // libx264, h264_nvenc, h264_vaapi
-            .with_codec_name("libx264".to_string());
+    let settings = Settings::preset_h264_yuv420p(1280, 720, false)
+        // libx264, h264_nvenc, h264_vaapi
+        .with_codec_name("libx264".to_string());
 
     let mut encoder = EncoderBuilder::new(Path::new("rainbow.mp4"), settings)
         .with_format("mp4")
