8eeb1e9d62715720df17741d795abd85a263033b
phial3 <27754514+phial3@users.noreply.github.com>
2025-02-27 10:04:28 +0800

    update encode flush

--- src/encode.rs
+++ src/encode.rs
@@ -422,19 +422,17 @@ impl Encoder {
 
     /// Flush the encoder, drain any packets that still need processing.
     fn flush(&mut self) -> Result<()> {
-        // Maximum number of invocations to `encoder_receive_packet`
-        // to drain the items still on the queue before giving up.
-        const MAX_DRAIN_ITERATIONS: u32 = 100;
-
         // Notify the encoder that the last frame has been sent.
-        self.send_eof()?;
+        self.send_eof().context("Send EOF frame failed.")?;
 
-        // We need to drain the items still in the encoders queue.
-        for _ in 0..MAX_DRAIN_ITERATIONS {
+        loop {
             match self.encoder_receive_packet() {
-                Ok(Some(packet)) => self.write(packet).unwrap(),
-                Ok(None) => continue,
-                Err(_) => break,
+                Ok(Some(packet)) => self.write(packet)?,
+                Ok(None) => {
+                    log::debug!("No more packet received.");
+                    break;
+                }
+                Err(e) => return Err(e).context("Receive packet failed.")?,
             }
         }
 
