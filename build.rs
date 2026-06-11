94a00cafcf178468c3220546b6aa6b7e7885b7cd
phial3 <feiyan.affection@gmail.com>
2026-06-11 15:04:12 +0800

    add env:LD_LIBRARY_PATH

--- build.rs
+++ build.rs
@@ -149,37 +149,6 @@ fn configure_windows(target_arch: &str) {
         println!("cargo:rustc-link-arg=/DEFAULTLIB:msvcrt.lib");
     }
 
-    #[cfg(target_os = "windows")]
-    {
-        let ffmpeg = vcpkg::find_package("ffmpeg")
-            .expect("Failed to find ffmpeg libs by vcpkg, please ensure vcpkg is installed.");
-
-        // 定义一个函数来处理库名称提取和链接
-        let rustc_link_libs = |libs: &[PathBuf], lib_type: &str| {
-            for lib in libs {
-                // 库的基本名称，不包含路径、前缀（如 "lib"）或扩展名（如 ".dll"、".lib"）
-                let lib_name = lib.file_stem().unwrap_or_default().to_string_lossy();
-                // 如果名称包含连字符（如 "avcodec-61"），只保留连字符前的部分
-                let lib_base_name = if let Some(idx) = lib_name.find('-') {
-                    lib_name[..idx].to_string()
-                } else {
-                    lib_name.to_string()
-                };
-                println!("cargo:rustc-link-lib={lib_type}={lib_base_name}");
-            }
-        };
-
-        if ffmpeg.is_static {
-            rustc_link_libs(&ffmpeg.found_libs, "static");
-        } else {
-            rustc_link_libs(&ffmpeg.found_dlls, "dylib");
-        }
-
-        for path in ffmpeg.link_paths {
-            println!("cargo:rustc-link-search=native={}", path.display());
-        }
-    }
-
     // Windows 系统库
     let system_libs = [
         // 基础系统库
